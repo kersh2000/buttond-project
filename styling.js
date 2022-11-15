4906ca5c101752bca06d9fde8c93ecacb9d078ef
kersh2000 <bradleykershaw333@gmail.com>
2022-11-15 16:31:28 +0000

    Second Commit

---
+++ styling.js
@@ -0,0 +1,141 @@
+class Styling {
+
+  static finalStyling = 'margin: 10px; font-size: 20px;';
+  static styleCounter = 0;
+  static numOfRandBtns;
+  static cssStyles;
+  static colors = ['black', 'white'];
+  static colorCounter = 0;
+  static stylesArr;
+
+  static getFancyStyling(styling) {
+    let fancyText = "";
+    let counter = 0;
+    for (let chr of styling) {
+      counter ++;
+      fancyText += chr;
+      if (chr === '{' || chr === ';') {
+        fancyText += '<br>';
+        if (counter !== styling.length - 1) {
+          fancyText += '&emsp;';
+        }
+      }
+    }
+    return fancyText;
+  }
+  
+  static randomColor() {
+    let color;
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    if (Styling.colors.length === 0) {
+      color = randomColor;
+    } else {
+      const picked = Styling.colors[0];
+      Styling.colors.splice(Styling.colors.indexOf(picked), 1);
+      color = picked;
+    }
+    Styling.colorCounter ++;
+    if (Styling.colorCounter === Styling.numOfRandBtns) {
+      Styling.colors = ['black', 'white']
+      Styling.colorCounter = 0;
+    }
+    return color;
+  }
+
+  constructor(numOfRandBtns) {
+    Styling.cssStyles = {
+      'background-color': [Styling.randomColor],
+      'color': [Styling.randomColor],
+      'border-style': ['solid', 'none', 'dotted', 'dashed'],
+      'border-color': [Styling.randomColor],
+      'border-width': ['0px', '1px', '2px', '3px', '4px', '5px'],
+      'border-radius': ['none', '3px', '7px', '11px', '100vw'],
+      'box-shadow': [this.randomBoxShadow],
+      'padding': [this.paddingH, 10],
+      'padding2': [this.paddingV, 10]
+    };
+    Styling.numOfRandBtns = numOfRandBtns;
+  }
+
+  paddingH() {
+    const arr = ['0px 0px', '0px 2px', '0px 5px', '0px 8px', '0px 12px', '0px 15px', '0px 20px', '0px 30px', '0px 50px', '0px 100px'];
+    return arr[Styling.stylesArr.length];
+  }
+
+  paddingV() {
+    const sub1 = Styling.finalStyling.slice(Styling.finalStyling.indexOf('padding: 0px ') + 13);
+    const paddingSides = sub1.slice(0, -1);
+    const arr = [`0px ${paddingSides}`, `2px ${paddingSides}`, `5px ${paddingSides}`, `8px ${paddingSides}`, `12px ${paddingSides}`, `15px ${paddingSides}`, `20px ${paddingSides}`, `30px ${paddingSides}`, `50px ${paddingSides}`, `100px ${paddingSides}`];
+    return arr[Styling.stylesArr.length];
+  }
+
+  randomBoxShadow() {
+    const color = Styling.randomColor();
+    const hOffset = (2 + Math.floor(Math.random() * 5)) + 'px';
+    const vOffset = (2 + Math.floor(Math.random() * 5)) + 'px';
+    const blur = Math.floor(Math.random() * 6) + 'px';
+    return `${hOffset} ${vOffset} ${blur} ${color}`;
+  }
+
+  randomInteger(min, max, end) {
+    const randomNum = `${Math.floor(Math.random() * ((max + 1) - min)) + min} ${end}`;
+    return randomNum;
+  }
+
+  addButtons(container) {
+    container.innerHTML = '';
+    const styles = this.getAttrValues();
+    styles.forEach((style, index) => {
+      container.innerHTML += `
+      <div class="rand-ctr-${index + 1}" style="background-color: white; color: black;">
+        <button id="btn-${index + 1}" class="rand-btn" 
+          style="${style}">
+        Click Here!
+        </button>
+        <p>Random Button ${index + 1}</p>
+      </div>
+      `;
+    });
+  }
+
+  getAttrValues() {
+    Styling.stylesArr = new Array();
+    const name = Object.keys(Styling.cssStyles)[Styling.styleCounter];
+    const values = Styling.cssStyles[name];
+    
+    for (let i = 0; i < values.length; i++) {
+      let value = values[i];
+      if (values.length === 1) {
+        i = -1;
+        if (Styling.numOfRandBtns === Styling.stylesArr.length) {
+          break;
+        }
+        value = value();
+      } else if (typeof(values[1]) === 'number') {
+        i = -1;
+        if (values[1] === Styling.stylesArr.length) {
+          break;
+        }
+        value = value();
+      }
+      const attr = ` ${name.replace(/[0-9]/g, '')}: ${value};`;
+      const styling = `${Styling.finalStyling} ${attr}`;
+      Styling.stylesArr.push(styling);
+    }
+    return Styling.stylesArr;
+  }
+
+  addAttr(btn) {
+    Styling.finalStyling = btn.getAttribute('style');
+    this.removeDups();
+    if (Styling.styleCounter === 2 && btn.style.borderStyle === 'none') {
+      Styling.styleCounter += 2;
+    }
+  }
+
+  removeDups() {
+    console.log(`finalStyling = ${Styling.finalStyling}`);
+  }  
+}
+
+export default Styling;
