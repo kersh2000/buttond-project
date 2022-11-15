b0b6c0709c09a7b5be49c03652f44eaedea34dbf
kersh2000 <bradleykershaw333@gmail.com>
2022-11-15 11:29:55 +0000

    Added 'finalButton' function for end of page.

--- step/index.js
+++ step/index.js
@@ -1,14 +1,15 @@
 const numOfRandButtons = 50;
-let finalStyling = '';
+const mainContainer = document.querySelector('#buttons-container');
+let finalStyling = 'margin: 10px; font-size: 20px;';
 let counter = 0;
 const cssStyles = {
-  'background-color': ['random'],
-  'color': ['random'],
+  'background-color': [randomColor],
+  'color': [randomColor],
   'border-style': ['solid', 'none'],
-  'border-color': ['random'],
+  'border-color': [randomColor],
   'border-width': ['0px', '1px', '2px', '3px'],
   'border-radius': ['0px', '0px', '7px', '100vw'],
-  'box-shadow': ['box-shadow'],
+  'box-shadow': [randomBoxShadow],
 };
 
 function randomColor() {
@@ -25,22 +26,18 @@ function randomBoxShadow() {
 }
 
 function addRandomButtons(num) {
-  const mainContainer = document.querySelector('#buttons-container');
+  mainContainer.innerHTML = '';
   for (let i = 0; i < numOfRandButtons; i++) {
-    let styling = "";
     const attr = Object.keys(cssStyles)[num];
     const values = cssStyles[attr];
     let value = values[Math.floor(Math.random() * values.length)];
-    if (value === 'random') {
-      value = randomColor();
-    } else if (value === 'box-shadow') {
-      value = randomBoxShadow();
+    if (typeof(value) === 'function') {
+      value = value();
     }
-    styling += ` ${attr}: ${value};`;
+    const styling = ` ${attr}: ${value};`;
     mainContainer.innerHTML += `
       <div class="rand-ctr-${i + 1}" style="background-color: white; color: black;">
-        <button id="btn-${i}" onclick="next(this.id)" class="rand-btn-${i + 1}" style="margin: 10px; font-size: 20px;
-        ${styling} ${finalStyling}">
+        <button id="btn-${i + 1}" onclick="next(this.id)" class="rand-btn-${i + 1}" style="${styling} ${finalStyling}">
         Click Here!
         </button>
         <p>Randm Button ${i + 1}</p>
@@ -49,13 +46,52 @@ function addRandomButtons(num) {
   }
 }
 
+function finalButton() {
+  mainContainer.innerHTML = '';
+  mainContainer.innerHTML += `
+    <div class="final-ctr" style="background-color: white; color: black;">
+      <button id="final-btn" class="final-btn" onclick="addButtonDB()" style="${finalStyling}">
+      Click Here!
+      </button>
+      <p>You're Button!</p>
+    </div>
+    `;
+}
+
+function addButtonDB() {
+  const styling = `button_name { ${finalStyling}}`;
+  const style = fancy(styling);
+  document.querySelector('body').innerHTML += `<p class="button-css">${style}</p>`;
+}
+
+function fancy(styling) {
+  let text = "";
+  let counter = 0;
+  for (let chr of styling) {
+    counter ++;
+    text += chr;
+    if (chr === '{' || chr === ';') {
+      text += '<br>';
+      if (counter !== styling.length - 1) {
+        text += '&emsp;';
+      }
+    }
+  }
+  return text;
+}
+
 addRandomButtons(counter);
 
-function next(id) {
+function next(id, skip = false) {
+  console.log(`#${id}`);
   const btn = document.querySelector(`#${id}`);
-  finalStyling = btn.getAttribute('style');
-  counter ++;
-  const mainContainer = document.querySelector('#buttons-container');
-  mainContainer.innerHTML = '';
+  if (!skip) {
+    finalStyling = btn.getAttribute('style');
+    counter ++;
+    if (counter === Object.keys(cssStyles).length) {
+      finalButton();
+      return;
+    }
+  }
   addRandomButtons(counter)
 }
