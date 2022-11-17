0ea9a139760920384888454d94b03b05b9cdb447
kersh2000 <bradleykershaw333@gmail.com>
2022-11-17 00:33:48 +0000

    Added cors library to use http requests not origin

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 require('./db/connection');
 const express = require('express');
 const userRouter = require('./routes/user');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(cors());
 
 app.use('/user', userRouter);
 
